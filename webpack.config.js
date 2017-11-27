1feb9f17fad77f91fd05b43565bf69b93a0bb8f4
Huafeng Huang <huafeng.huang@gmail.com>
2017-11-27 15:27:33 +0800

    try

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
       __DARWIN__: process.platform === 'darwin',
       __WIN32__: process.platform === 'win32',
       __LINUX__: process.platform === 'linux',
-      __DEV__: process.env.NODE_ENV !== 'production',
+      __DEV__: process.env.ELECTRON_WEBPACK_APP_MODE === 'development',
     }),
   ],
   devtool: process.env.ELECTRON_WEBPACK_APP_MODE === 'development' ? 'inline-source-map' : false,
